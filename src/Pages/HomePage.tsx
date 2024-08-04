74e4b4f3484591f691129b4f1596e08ce38e2778
dinashkenazi <dinashkenazi@gmail.com>
2024-08-04 20:20:57 +0300

    email test3

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -382,6 +382,27 @@ function HomePage() {
         </p>
         <p>
           <button type="submit">Send</button>
+          <form
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </p>
       </form>
       <CookieConsentPopup />
