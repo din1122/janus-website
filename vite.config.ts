97c2dff27012314a95f6298c03dd7e5ec21aa228
Dinashkenazi <134402932+Dinashkenazi@users.noreply.github.com>
2024-08-09 20:23:48 +0300

    about us and styled components

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dotenv from 'dotenv';
-// https://vitejs.dev/config/
 
 dotenv.config();
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      plugins: [
+        ['@swc/plugin-styled-components', {
+          displayName: true,
+          fileName: true,
+          ssr: false,
+          pure: true,
+        }]
+      ]
+    }),
+  ],
   resolve: {
     alias: {
       '~': "/src",
