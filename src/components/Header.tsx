37bc513f2264ff2c64ff857599ed45304d85aa44
dinashkenazi <dinashkenazi@gmail.com>
2024-08-10 02:59:06 +0300

    comment unused components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,13 @@ import { FlexRow, FlexRowSpaceBetween } from '~/shared/flexes';
 import JanusLogo from '~/assets/janus.svg';
 import { pageLayout } from '~/shared/LayoutStyle';
 import { Link } from 'react-router-dom';
-import {
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem
-} from '@nextui-org/dropdown';
-import { useState } from 'react';
+// import {
+//   Dropdown,
+//   DropdownTrigger,
+//   DropdownMenu,
+//   DropdownItem
+// } from '@nextui-org/dropdown';
+// import { useState } from 'react';
 
 const StyledHeader = styled.div`
   display: flex;
@@ -31,40 +31,40 @@ const StyledHeader = styled.div`
   }
 `;
 
-const HeaderLinks = styled(Link)``;
-
-const DropdownItemStyled = styled(DropdownItem)`
-  padding: 12px;
-  background: black;
-`;
-
-const DropdownMenuPopup = styled(DropdownMenu)`
-  background: white;
-  padding: 12px;
-  color: black;
-  transform: translate(67px, 0px);
-  & > ul {
-    display: flex;
-    flex-direction: column;
-    &:focus-visible {
-      outline: none;
-    }
-  }
-  border-radius: 8px;
-  & * > a {
-    padding: 12px;
-    color: #52537a;
-    font-size: 14px;
-    &:focus-visible {
-      outline: none;
-    }
-    &:hover {
-      /* outline: 1px solid black; */
-      background: #f0f0f5;
-      border-radius: 4px;
-    }
-  }
-`;
+// const HeaderLinks = styled(Link)``;
+//
+// const DropdownItemStyled = styled(DropdownItem)`
+//   padding: 12px;
+//   background: black;
+// `;
+//
+// const DropdownMenuPopup = styled(DropdownMenu)`
+//   background: white;
+//   padding: 12px;
+//   color: black;
+//   transform: translate(67px, 0px);
+//   & > ul {
+//     display: flex;
+//     flex-direction: column;
+//     &:focus-visible {
+//       outline: none;
+//     }
+//   }
+//   border-radius: 8px;
+//   & * > a {
+//     padding: 12px;
+//     color: #52537a;
+//     font-size: 14px;
+//     &:focus-visible {
+//       outline: none;
+//     }
+//     &:hover {
+//       /* outline: 1px solid black; */
+//       background: #f0f0f5;
+//       border-radius: 4px;
+//     }
+//   }
+// `;
 
 const Header = () => {
   // const [isOpen, setIsOpen] = useState(true);
