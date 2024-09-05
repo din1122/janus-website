776c1b790d67bf217a4ece107724200e48fbfd0f
dinashkenazi <dinashkenazi@gmail.com>
2024-09-05 18:53:22 +0300

    deleted file

--- src/components/JanusLogo.tsx/index.spec.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import JanusLogo.tsx from '.';
-
-test('renders JanusLogo.tsx', () => {
-  render(<JanusLogo.tsx />);
-});
